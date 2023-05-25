34ee76aaa2e10a34cd1fc8c5abd3be91abe34fcf
Alberto <a.tiboni@gmail.com>
2023-05-25 15:58:48 +0200

    hiding context bar

--- src/app/components/template/Template.jsx
+++ src/app/components/template/Template.jsx
@@ -1,10 +1,10 @@
-import React, { useContext } from 'react';
+import React /*, { useContext }*/ from 'react';
 
 import { Navbar } from './components/Navbar.jsx';
 import { Sidebar } from './components/Sidebar.jsx';
-import { ContextBarSelector } from '../shared/ContextBarSelector.jsx';
+// import { ContextBarSelector } from '../shared/ContextBarSelector.jsx';
 
-import AnalysisContext from '../../context/analysisContext';
+// import AnalysisContext from '../../context/analysisContext';
 
 import '../../../index.scss';
 
@@ -14,24 +14,24 @@ const Template = props => {
 
     const contentClassNames = ['template-content'];
 
-    const { isContextBarVisible, toggleContextBar } = useContext(AnalysisContext);
+    // const { isContextBarVisible, toggleContextBar } = useContext(AnalysisContext);
 
-    if (!props.hiddenContextBar && isContextBarVisible) {
-        contentClassNames.push('template-content__with-contextBar');
-    }
+    // if (!props.hiddenContextBar && isContextBarVisible) {
+    //     contentClassNames.push('template-content__with-contextBar');
+    // }
 
     return (
         <div className="template-root">
             <Navbar />
             <Sidebar />
-            {
+            {/* {
                 !props.hiddenContextBar && (
                     <ContextBarSelector
                         visible={isContextBarVisible}
                         toggleBar={toggleContextBar}
                     />
                 )
-            }
+            } */}
             <div className={contentClassNames.join(' ')}>
                 {props.children}
             </div>
